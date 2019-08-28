86559b7bea8fc300c828a5446cdd8d1e7142d58e
David Perez <d.perez89@aol.com>
2019-08-28 08:27:56 -0500

    Generated forms on the Create page to capture data for the bound book property on the Create page model. Also added an OnPost method that validates model state and writes captured data to the database. Also added client-side validation for book data entry.

--- BookListRazor/Pages/BookList/Create.cshtml.cs
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +10,31 @@ namespace BookListRazor.Pages.BookList
 {
     public class CreateModel : PageModel
     {
+
+        public readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
         public void OnGet()
         {
 
+        }
+        //Because the book property above is already binded, we do not have to pass in a new book object in the OnPost method below.
+        public async Task<IActionResult> OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Book.Add(Book);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
         }
     }
 }
