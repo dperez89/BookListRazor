ca03a5d7ac2b3ba16a12881c7c87c77655762666
David Perez <d.perez89@aol.com>
2019-08-27 21:30:58 -0500

    Generated AddBookToDatabase migration and updated database with new Book table.

--- BookListRazor/Startup.cs
+++ BookListRazor/Startup.cs
@@ -33,7 +33,7 @@ namespace BookListRazor
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
